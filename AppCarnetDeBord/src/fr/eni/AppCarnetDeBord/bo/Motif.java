20210671d2da415d0090fe138c4e3fe5b13fdd4f
Administrateur <Administrateur@WINDOWS-IC7JNF9>
2018-02-14 11:14:04 +0100

    motif

--- AppCarnetDeBord/src/fr/eni/AppCarnetDeBord/bo/Motif.java
+++ AppCarnetDeBord/src/fr/eni/AppCarnetDeBord/bo/Motif.java
@@ -13,6 +13,27 @@ public class Motif {
 		super();
 		this.nomMotif = nomMotif;
 	}
+
+	public int getIdMotif() {
+		return idMotif;
+	}
+
+	public void setIdMotif(int idMotif) {
+		this.idMotif = idMotif;
+	}
+
+	public String getNomMotif() {
+		return nomMotif;
+	}
+
+	public void setNomMotif(String nomMotif) {
+		this.nomMotif = nomMotif;
+	}
+
+	@Override
+	public String toString() {
+		return "Motif [idMotif=" + idMotif + ", nomMotif=" + nomMotif + "]";
+	}
 	
 	
 	
