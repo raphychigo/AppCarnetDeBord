eddf570a14739638e34bf5795d9792e5f5ebd763
Administrateur <Administrateur@WINDOWS-6AH7ML5.FORM-NANTES.LOCAL>
2018-02-14 11:35:52 +0100

    Login/Logout

--- AppCarnetDeBord/src/fr/eni/AppCarnetDeBord/Servlet/LoginServlet.java
+++ AppCarnetDeBord/src/fr/eni/AppCarnetDeBord/Servlet/LoginServlet.java
@@ -1,11 +1,19 @@
 package fr.eni.AppCarnetDeBord.Servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.AppCarnetDeBord.bo.Utilisateur;
+import fr.eni.AppCarnetDeBord.dal.DAOUtilisateur;
+import fr.eni.ecole.jeeproject.bo.Animateur;
+import fr.eni.ecole.jeeproject.dal.AnimateurDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -26,7 +34,20 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try{
+			HttpSession session = request.getSession();
+			
+			if(session.getAttribute("id")!=null){
+				response.sendRedirect("/AppCarnetDeBord/Accueil");
+			}else{
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Conducteur/Menu");
+				if(dispatcher!=null){
+					dispatcher.include(request, response);
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -34,7 +55,25 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		try{
+			HttpSession session = request.getSession();
+			
+			if(session.getAttribute("id")!=null){
+				response.sendRedirect("/AppCarnetDeBord/Conducteur");
+			}else{
+				Utilisateur user =  null;
+			    user =DAOUtilisateur.selectUser(request.getParameter("email"),request.getParameter("password"));			    
+			    if(user!=null){
+			    	session.setMaxInactiveInterval(600);
+			    	session.setAttribute("id", user.getId());
+			    	response.sendRedirect("/AppCarnetDeBord/Conducteur/");
+			    }else{
+			    	request.getRequestDispatcher("/Login/ErreurConnexion").forward(request, response);
+			    }
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
