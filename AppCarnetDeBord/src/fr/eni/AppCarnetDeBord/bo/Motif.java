2703b1d28674377846be4549b1afbece2d1d9950
Administrateur <Administrateur@WINDOWS-IC7JNF9>
2018-02-14 11:13:36 +0100

    DAOVehicule Motif Veheicule

---
+++ AppCarnetDeBord/src/fr/eni/AppCarnetDeBord/bo/Motif.java
@@ -0,0 +1,20 @@
+package fr.eni.AppCarnetDeBord.bo;
+
+public class Motif {
+	private int idMotif;
+	private String nomMotif;
+	
+	
+	public Motif() {
+		super();
+	}
+	
+	public Motif(String nomMotif) {
+		super();
+		this.nomMotif = nomMotif;
+	}
+	
+	
+	
+	
+}
